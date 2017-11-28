5bbb1db3a8c7e740979dbaeb7f7caa344b4e5e0c
Glitch (reader-api) <none>
2017-11-28 02:24:31 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const got = require('got');
 const cheerio = require('cheerio');
+const moment = require('moment');
 const map = require('p-map');
 
 const Koa = require('koa');
@@ -10,27 +11,22 @@ const app = new Koa();
 
 app.use(cors())
 
-const toTimestamp = str => (new Date(Date.parse(str))).valueOf();
+const toTimestamp = dateString => moment(dateString, 'MM/DD/YY').unix();
 
-async function fetchManga (id) {
+async function fetchMangaMetadata (id) {
   const res = await got(`https://www.mangaupdates.com/releases.html?search=${id}&stype=series`);
   const html = res.body;
   const dom = cheerio.load(html);
   
-  const title = dom('td.text.pad[bgcolor]:nth-child(2)', '#main_content').text();
-  const updatedAtDate = dom('td.text.pad[bgcolor]:nth-child(1)', '#main_content').text();
+  const title = dom('td.text.pad[bgcolor]:nth-child(2)', '#main_content').first().text();
+  const updatedAtDate = dom('td.text.pad[bgcolor]:nth-child(1)', '#main_content').first().text();
   const updatedAt = toTimestamp(updatedAtDate);
-  
-  console.log(dom('#main_content > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3)').text());
   
   return { id, title, updatedAt };
 }
 
 app.use(route.get('/manga/:id', async (ctx, id) => {
-  const results = await fetchManga(id);
-  ctx.body = results;
+  ctx.body = await fetchMangaMetadata(id);
 }));
-
-fetchManga(130971).then(res => console.log(res));
 
 app.listen(process.env.PORT);
