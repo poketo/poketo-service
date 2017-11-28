b03ec891a50788ffd58ace0554a125d000e31207
Glitch (reader-api) <none>
2017-11-28 01:30:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 const got = require('got');
 const cheerio = require('cheerio');
 const map = require('p-map');
+const cors = require('cors');
 
 const express = require('express');
 const app = express();
 
-const UPDATED_AT_SELECTOR = '#main_content > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1)';
+const selectors = {
+  title: '#main_content > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(2)',
+  updatedAt: '#main_content > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1)',
+};
 
-app.get('/', (req, res) => {
-  res.sendStatus(404);
-});
+app.use(cors())
 
-async function checkUpdatedAt (id) {
+const toTimestamp = str => (new Date(Date.parse(str))).valueOf();
+
+async function fetchManga (id) {
   const res = await got(`https://www.mangaupdates.com/releases.html?search=${id}&stype=series`);
   const html = res.body;
   const dom = cheerio.load(html);
   
-  const updatedAt = dom(UPDATED_AT_SELECTOR).text();
-  const timestamp = (new Date(Date.parse(updatedAt))).valueOf();
+  const title = dom(selectors.title).text();
+  const updatedAtDate = dom(selectors.updatedAt).text();
+  const updatedAt = toTimestamp(updatedAtDate);
   
-  return timestamp;
+  console.log(title, updatedAtDate);
+  
+  return { id, title, updatedAt };
 }
 
-app.get('/:mangaId', (req, res) => {
-  res.send(dreams);
+app.get('/manga/:mangaId', async (req, res) => {
+  const results = await fetchManga(req.params.mangaId);
+  res.json(results);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
